806055ec5d6f51745a1be3aa6daf299f48df0c80
Jefferson <fjeffersonpineda@hotmail.com>
2020-05-18 21:40:35 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,37 @@
+//reuires
+const express = require('express');
+//importacion
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+
+//obtener el puesto de heroku
+const port = process.env.PORT || 3000;
+
+//middleware sirve para que express sepa cual folder queremos servir(que se muestre)
+//con esto hacemos publica la carpeta
+app.use(express.static(__dirname + '/public'));
+
+//EXPRESS HBS engine
+//antes ejecutar npm install hbs --save
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//pagina principal
+app.get('/', (req, res) => {
+    res.render('home', { //con el objeto envio la info a la pagina
+        nombre: 'Jefferson Pineda'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { //con el objeto envio la info a la pagina
+    });
+});
+
+//app.listen(3000)
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
